7c313a5f31e8c1f92d609dfc65c6feb31954a647
Kamar <kamar.ali.contractor@dvla.gov.uk>
2019-11-13 00:49:02 +0000

    MVP stuff

---
+++ DeployToLamda.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+cd ./trees-calculator
+rm *.zip
+zip -r foo.zip .
+cd ..
+
+#aws lambda update-function-code --function-name babs-backend --zip-file fileb://foo.zip
+
+echo "Done"
---
+++ trees-calculator/index.js
@@ -0,0 +1,100 @@
+'use strict';
+const Alexa = require('ask-sdk-core');
+var treeConsumptionCalculator = require('./treeConsumptionCalculator.js');
+// use 'ask-sdk' if standard SDK module is installed
+
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+    },
+    handle(handlerInput) {
+        const speechText = 'Welcome to the Alexa Skills Kit, you can say hello!';
+return handlerInput.responseBuilder
+            .speak(speechText)
+            .reprompt(speechText)
+            .withSimpleCard('Hello World', speechText)
+            .getResponse();
+    }
+};
+
+const HelloWorldIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'OrderFlowers';
+    },
+    async handle(handlerInput) {
+      let destA = handlerInput.requestEnvelope.request.intent.slots.destA.value
+      let destB = handlerInput.requestEnvelope.request.intent.slots.destB.value
+      let carReg = handlerInput.requestEnvelope.request.intent.slots.carReg.value
+
+
+      let data = await treeConsumptionCalculator.handler(destA, destB, carReg)
+      console.log("daaaattattataa, ", data)
+
+      return handlerInput.responseBuilder
+                  .speak(data)
+                  .withSimpleCard('Hello World', "hello kamar I will work eventually don't worry")
+                  .getResponse();
+    }
+};
+
+const HelpIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
+    },
+    handle(handlerInput) {
+        const speechText = 'You can say hello to me!';
+return handlerInput.responseBuilder
+            .speak(speechText)
+            .reprompt(speechText)
+            .withSimpleCard('Hello World', speechText)
+            .getResponse();
+    }
+};
+
+const CancelAndStopIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
+                || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
+    },
+    handle(handlerInput) {
+        const speechText = 'Goodbye!';
+return handlerInput.responseBuilder
+            .speak(speechText)
+            .withSimpleCard('Hello World', speechText)
+            .getResponse();
+    }
+};
+
+const SessionEndedRequestHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        //any cleanup logic goes here
+        return handlerInput.responseBuilder.getResponse();
+    }
+};
+
+const ErrorHandler = {
+    canHandle() {
+      return true;
+    },
+    handle(handlerInput, error) {
+      console.log(`Error handled: ${error.message}`);
+return handlerInput.responseBuilder
+        .speak('Sorry, I can\'t understand the command. Please say again.')
+        .reprompt('Sorry, I can\'t understand the command. Please say again.')
+        .getResponse();
+    },
+};
+
+exports.handler = Alexa.SkillBuilders.custom()
+     .addRequestHandlers(LaunchRequestHandler,
+                         HelloWorldIntentHandler,
+                         HelpIntentHandler,
+                         CancelAndStopIntentHandler,
+                         SessionEndedRequestHandler)
+     .lambda();
